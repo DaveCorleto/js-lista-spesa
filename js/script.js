15c72162c72cddceb0f9ae214d3e916213d802b1
DaveCorleto <davide.corleto@fastwebnet.it>
2023-11-24 14:51:24 +0100

    Config page and Boilerplate

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let shoppingList = ["uova", "zucca castagna", "olive", "salame nostrano", "provola", "cavolo nero", "cime di rapa", "spazzolino", "carta igienica", "gelato", "caffè"]
+
+// Ciao ragazze e ciao ragazzi!
+// Come annunciato oggi ci sarà un recap nel pomeriggio, perciò l’esercizio sarà piccino.
+// Esercizio di oggi: Lista della spesa con ciclo while
+// nome repo: js-lista-spesa
+
+// Consegna:
+
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+// Consigli:
+// - Ricordiamoci di inizializzare la variabile di contatore prima dell’inizio del ciclo while
+// - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all’interno ciclo while
+
+// volendo BONUS:
+// do la possibilità all’utente di inserire voci nella lista
+
+let shoppingItem = [i];
+
+while ()
+
+console.log ()
